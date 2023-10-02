1f6ad16c86aae1ddafcfda9efc800fb3ac4d3aaa
kgrtmno0507 <47068997+kgrtmno0507@users.noreply.github.com>
2023-10-02 21:40:08 +0900

    Initial commit

---
+++ src/components/CompleteTodos.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const style = {
+  backgroundColor: "#ff99e0",
+  width: "400px",
+  minHeight: "200px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+};
+
+export const CompleteTodos = (props) => {
+  const { todos, onClickReturn } = props;
+  return (
+    <div style={style}>
+      <p className="title">完了のTODO</p>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <div key={todo} className="list-row">
+              <li>{todo}</li>
+              <button onClick={() => onClickReturn(index)}>戻す</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
